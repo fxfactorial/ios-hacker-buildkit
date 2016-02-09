522e13d50d0cbfabf12ccc55a7b9482ce0417364
Edgar Aroutiounian <edgar.factorial@gmail.com>
2016-02-09 12:40:56 -0800

    Add namespaces, start add Logos hook rule

--- src/buildkit.cpp
+++ src/buildkit.cpp
@@ -10,90 +10,123 @@
 #include <folly/Format.h>
 #include <folly/FileUtil.h>
 
-#include <boost/config/warning_disable.hpp>
 #include <boost/spirit/include/qi.hpp>
 #include <boost/spirit/include/phoenix_operator.hpp>
-
-folly::dynamic project_spec = nullptr;
-
-std::string getcwd_string(void)
-{
-  char buff[PATH_MAX];
-  getcwd(buff, PATH_MAX);
-  std::string cwd(buff);
-  return cwd;
-}
-
-void check_and_set_spec(const std::string project_path)
-{
-  if (project_spec == nullptr) {
-
-    std::string load_pkg_desc;
-    std::string full_path =
-      folly::sformat("{}/{}", getcwd_string(), project_path);
-
-    if (!folly::readFile(full_path.data(), load_pkg_desc)) {
-      exit(-1);
-    }
-    project_spec = folly::parseJson(load_pkg_desc);
-  }
-}
+#include <boost/config/warning_disable.hpp>
+#include <boost/fusion/include/adapt_struct.hpp>
 
 namespace logos
 {
   namespace qi = boost::spirit::qi;
   namespace ascii = boost::spirit::ascii;
 
-  struct directives_ : qi::symbols<char, std::string>
+  struct class_hook
   {
-    directives_()
+    std::string class_name;
+    std::string method_signature;
+    std::string method_body;
+  };
+
+  template <typename Iterator>
+  struct class_hook_parser : qi::grammar<Iterator, class_hook(), ascii::space_type>
+  {
+    class_hook_parser() : class_hook_parser::base_type(start)
     {
-      add
-	("C"    , "1234")
-	("CM"   , "1235")
-	("CM"   , "1235")
+      using qi::int_;
+      using qi::lit;
+      using qi::double_;
+      using qi::lexeme;
+      using ascii::char_;
 
+      quoted_string %= lexeme['"' >> +(char_ - '"') >> '"'];
+
+      start %=
+	lit("%hook")
+	>>  *(char_)
+	>> '-'
 	;
-    }
 
-  } directives;
+    }
+    qi::rule<Iterator, std::string(), ascii::space_type> quoted_string;
+    qi::rule<Iterator, class_hook_parser(), ascii::space_type> start;
+
+  };
+
 }
 
+BOOST_FUSION_ADAPT_STRUCT(logos::class_hook,
+			  (std::string, class_name)
+			  (std::string, method_signature)
+			  (std::string, method_body))
 
+namespace buildkit {
+
+  namespace impl {
+    folly::dynamic project_spec = nullptr;
+
+    std::string getcwd_string(void)
+    {
+      char buff[PATH_MAX];
+      getcwd(buff, PATH_MAX);
+      std::string cwd(buff);
+      return cwd;
+    }
+
+    void check_and_set_spec(const std::string project_path)
+    {
+      if (project_spec == nullptr) {
+
+	std::string load_pkg_desc;
+	std::string full_path =
+	  folly::sformat("{}/{}", getcwd_string(), project_path);
+
+	if (!folly::readFile(full_path.data(), load_pkg_desc)) {
+	  exit(-1);
+	}
+	project_spec = folly::parseJson(load_pkg_desc);
+      }
+    }
+  }
+  char *build(const char *, int, char **argv)
+  {
+    impl::check_and_set_spec(std::string(*argv));
+    return NULL;
+  }
+
+  char *package(const char *func_name, int argc, char **argv)
+  {
+    impl::check_and_set_spec(std::string(*argv));
+    return NULL;
+  }
+
+  char *deploy(const char *func_name, int argc, char **argv)
+  {
+    impl::check_and_set_spec(std::string(*argv));
+    return NULL;
+  }
+
+}
 
 // And now we talk to Make
-
 extern "C" {
 
 #include <gnumake.h>
 
   int plugin_is_GPL_compatible;
 
-  // We give back null to each of these because they are just for side
-  // effects
-  char *build(const char *, int, char **argv)
-  {
-    check_and_set_spec(std::string(*argv));
-    return NULL;
-  }
-
-  char *package(const char *func_name, int argc, char **argv)
-  {
-    check_and_set_spec(std::string(*argv));
-    return NULL;
-  }
-
-  char *deploy(const char *func_name, int argc, char **argv)
-  {
-    check_and_set_spec(std::string(*argv));
-    return NULL;
-  }
-
+  // This convention does matter, func name ought to end with
+  // gmk_setup
   int buildkit_gmk_setup()
   {
-    gmk_add_function("tweak_builder", (gmk_func_ptr)build, 1, 1, 0);
-    gmk_add_function("tweak_package", (gmk_func_ptr)package, 1, 1, 0);
-    gmk_add_function("tweak_deploy", (gmk_func_ptr)deploy, 1, 1, 0);
+    // We say that we our builtin make functions shall take a min
+    // number of 1 argments and a max of 1 arguments, aka I really
+    // want that package.json to be there.
+    gmk_add_function("tweak_builder",
+		     (gmk_func_ptr)buildkit::build, 1, 1, 0);
+    gmk_add_function("tweak_package",
+		     (gmk_func_ptr)buildkit::package, 1, 1, 0);
+    gmk_add_function("tweak_deploy",
+		     (gmk_func_ptr)buildkit::deploy, 1, 1, 0);
     return 1;
   }
 
